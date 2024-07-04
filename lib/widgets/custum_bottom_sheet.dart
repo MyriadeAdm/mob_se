8610fe62eb8daf9c635c78d0546d1c8f859cbfaf
MyriadeAdm <kahar.idrissou@myriade-groupe.com>
2024-07-04 13:13:51 +0000

    Maintenance bottomSheet

--- lib/widgets/custum_bottom_sheet.dart
+++ lib/widgets/custum_bottom_sheet.dart
@@ -78,223 +78,226 @@ Future<void> callButtomSheet(BuildContext context) async {
     ),
     builder: ((context) {
       return Expanded(
-        child: StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState) {
-            return Padding(
-              padding: const EdgeInsets.only(
-                  right: 30, left: 30, top: 30, bottom: 80),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'credit',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Text('SMS',
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.w500)),
-                      Text('Validite ',
-                          style: TextStyle(
-                              fontSize: 13.5, fontWeight: FontWeight.w400)),
-                      SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Montant',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Text(
-                            'prix',
-                            style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.w500,
-                              //color: Colors.orange,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  const Divider(
-                    height: 30,
-                    thickness: 2,
-                    indent: 30,
-                    endIndent: 30,
-                  ),
-
-                  Directionality(
-                    textDirection: TextDirection.rtl,
-                    child: LabeledCheckbox(
-                      label: 'A une autre personne',
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      value: _isSelected,
-                      onChanged: (bool newValue) {
-                        setState(() {
-                          _isSelected = newValue;
-                          numVisible = !numVisible;
-                        });
-                      },
-                    ),
-                  ),
-
-                  Visibility(
-                    visible: numVisible,
-                    child: Row(
+        child: Padding(
+          padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return Padding(
+                padding: const EdgeInsets.only(
+                    right: 30, left: 30, top: 30, bottom: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Expanded(
-                          child: TextField(
-                            controller: _textController,
-                            decoration: InputDecoration(
-                              hintText: 'Nom ou numéro de téléphone',
-                              filled: true,
-                              fillColor: const Color.fromRGBO(230, 227, 227, 1),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide.none,
-                                borderRadius: BorderRadius.circular(10),
+                        Text(
+                          'credit',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text('SMS',
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.w500)),
+                        Text('Validite ',
+                            style: TextStyle(
+                                fontSize: 13.5, fontWeight: FontWeight.w400)),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Montant',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
                               ),
-                              suffixIcon: IconButton(
-                                onPressed: () {
-                                  _textController.clear();
-                                },
-                                icon: const Icon(
-                                  Icons.clear,
-                                  size: 20,
-                                  color: Colors.grey,
+                            ),
+                            Text(
+                              'prix',
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.w500,
+                                //color: Colors.orange,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    const Divider(
+                      height: 30,
+                      thickness: 2,
+                      indent: 30,
+                      endIndent: 30,
+                    ),
+          
+                    Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: LabeledCheckbox(
+                        label: 'A une autre personne',
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        value: _isSelected,
+                        onChanged: (bool newValue) {
+                          setState(() {
+                            _isSelected = newValue;
+                            numVisible = !numVisible;
+                          });
+                        },
+                      ),
+                    ),
+          
+                    Visibility(
+                      visible: numVisible,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: _textController,
+                              decoration: InputDecoration(
+                                hintText: 'Nom ou numéro de téléphone',
+                                filled: true,
+                                fillColor: const Color.fromRGBO(230, 227, 227, 1),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    _textController.clear();
+                                  },
+                                  icon: const Icon(
+                                    Icons.clear,
+                                    size: 20,
+                                    color: Colors.grey,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.contacts_rounded,
-                              color: Colors.grey, size: 40.0),
-                          onPressed: () {
-                            setState(() {
-                              click += 1;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  //const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Expanded(
-                          child: ListTile(
-                              title: const Text('Credit'),
-                              leading: Radio(
-                                value: options[0],
-                                groupValue: currentOption,
-                                onChanged: (value) {
-                                  setState(() {
-                                    currentOption = value.toString();
-                                    codeVisible = !codeVisible;
-                                  });
-                                },
-                                activeColor: Colors.amber,
-                              ))),
-                      Expanded(
-                          child: ListTile(
-                              title: const Text('T-money'),
-                              leading: Radio(
-                                value: options[1],
-                                groupValue: currentOption,
-                                onChanged: (value) {
-                                  setState(() {
-                                    currentOption = value.toString();
-                                    codeVisible = !codeVisible;
-                                  });
-                                },
-                                activeColor: Colors.amber,
-                              ))),
-                    ],
-                  ),
-                  //const SizedBox(height: 20),
-                  Visibility(
-                    visible: codeVisible,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Code',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18,
+                          IconButton(
+                            icon: const Icon(Icons.contacts_rounded,
+                                color: Colors.grey, size: 40.0),
+                            onPressed: () {
+                              setState(() {
+                                click += 1;
+                              });
+                            },
                           ),
-                        ),
-                        SizedBox(
-                          width: 200,
-                          height: 35,
-                          child: TextField(
-                            keyboardType: TextInputType.number,
-                            obscureText: true,
-                            //onChanged: (){},
-                            obscuringCharacter: "*",
-                            controller: TextEditingController(),
-                            decoration: InputDecoration(
-                              filled: true,
-                              //hintText: '',
-                              fillColor: const Color.fromRGBO(230, 227, 227, 1),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide.none,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              // suffixIcon: IconButton(
-                              //   onPressed: () {
-                              //     _textController.clear();
-                              //   },
-                              //   icon: const Icon(
-                              //     Icons.clear,
-                              //     size: 15,
-                              //     color: Colors.grey,
-                              //   ),
-                              // ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  SizedBox(
-                    height: 50,
-                    width: 180,
-                    //child: CustomButton(label: 'Confirmer'),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.amber,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          )),
-                      onPressed: () => {},
-                      child: const Text(
-                        'CONFIRMER',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.black,
-                        ),
+                        ],
                       ),
                     ),
-                  )
-                ],
-              ),
-            );
-          },
+                    //const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                            child: ListTile(
+                                title: const Text('Credit'),
+                                leading: Radio(
+                                  value: options[0],
+                                  groupValue: currentOption,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      currentOption = value.toString();
+                                      codeVisible = !codeVisible;
+                                    });
+                                  },
+                                  activeColor: Colors.amber,
+                                ))),
+                        Expanded(
+                            child: ListTile(
+                                title: const Text('T-money'),
+                                leading: Radio(
+                                  value: options[1],
+                                  groupValue: currentOption,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      currentOption = value.toString();
+                                      codeVisible = !codeVisible;
+                                    });
+                                  },
+                                  activeColor: Colors.amber,
+                                ))),
+                      ],
+                    ),
+                    //const SizedBox(height: 20),
+                    Visibility(
+                      visible: codeVisible,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Code',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 18,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 200,
+                            height: 35,
+                            child: TextField(
+                              keyboardType: TextInputType.number,
+                              obscureText: true,
+                              //onChanged: (){},
+                              obscuringCharacter: "*",
+                              controller: TextEditingController(),
+                              decoration: InputDecoration(
+                                filled: true,
+                                //hintText: '',
+                                fillColor: const Color.fromRGBO(230, 227, 227, 1),
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                // suffixIcon: IconButton(
+                                //   onPressed: () {
+                                //     _textController.clear();
+                                //   },
+                                //   icon: const Icon(
+                                //     Icons.clear,
+                                //     size: 15,
+                                //     color: Colors.grey,
+                                //   ),
+                                // ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      height: 50,
+                      width: 180,
+                      //child: CustomButton(label: 'Confirmer'),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.amber,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                        onPressed: () {},
+                        child: const Text(
+                          'CONFIRMER',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       );
     }),
