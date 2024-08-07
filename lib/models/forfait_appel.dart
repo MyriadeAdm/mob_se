01e0f88141429f32fdd0457487029324a666aa58
MyriadeAdm <kahar.idrissou@myriade-groupe.com>
2024-08-07 16:16:18 +0000

    7 aout 16h

--- lib/models/forfait_appel.dart
+++ lib/models/forfait_appel.dart
@@ -7,9 +7,11 @@ class ForfaitAppel {
   String prix;
   String codeMMCredit;
   String codeAutruiCredit;
+  String codeMoneyMM;
+  String codeMoneyAutruit;
   // ignore: prefer_typing_uninitialized_variables
   var mega;
   Typeforfait typeforfait = Typeforfait.forfaitAppel;
 
-  ForfaitAppel(this.credit, this.msg, this.validite, this.prix,  this.codeMMCredit, this.codeAutruiCredit, this.mega);
+  ForfaitAppel(this.credit, this.msg, this.validite, this.prix,  this.codeMMCredit, this.codeAutruiCredit, this.codeMoneyMM, this.codeMoneyAutruit, this.mega);
 }
