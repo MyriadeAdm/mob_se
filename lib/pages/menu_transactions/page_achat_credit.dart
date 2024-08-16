1dd4ae8dcf395130d73af0b9619bde5b68fb1a20
Myriade-P <jonas.konutse@myriade-groupe.com>
2024-08-16 15:31:28 +0000

    page achat credit

--- lib/pages/menu_transactions/page_achat_credit.dart
+++ lib/pages/menu_transactions/page_achat_credit.dart
@@ -1,4 +1,4 @@
-import 'package:dropdown_search/dropdown_search.dart';
+                  ],
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_contact_picker/flutter_native_contact_picker.dart';
