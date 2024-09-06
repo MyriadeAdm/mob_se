82539b536a96ceda1ba82ff715f101d9b52d04f1
MyriadeAdm <kahar.idrissou@myriade-groupe.com>
2024-09-06 12:15:46 +0000

    splash

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Center(
+      child: Container(
+        child: Lottie.asset("assets/json/Mobse.json",),
+      ),
+    );
+  }
+}
