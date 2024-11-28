ef2564cd4a3a7f252844eff23004037f9f32d494
MyriadeAdm <kahar.idrissou@myriade-groupe.com>
2024-11-28 10:30:25 +0000

    divider

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 ThemeData darkMode = ThemeData(
     colorScheme: const ColorScheme.dark(
       surface: Color.fromARGB(255, 0, 20, 43),
-      primary: Color.fromARGB(255, 174, 174, 174),
-      secondary: Color.fromARGB(255, 30, 30, 30),
+      primary: Color.fromARGB(255, 244, 244, 244),
+      secondary: Color.fromARGB(255, 0, 14, 31),
       tertiary: Color.fromARGB(255, 47, 47, 47),
       inversePrimary: Color.fromRGBO(243, 243, 243, 1.0),
     ),
