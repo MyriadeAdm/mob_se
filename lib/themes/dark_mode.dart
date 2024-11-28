28bdf26f392925ca239889f461a9a7c5ec85ec98
MyriadeAdm <kahar.idrissou@myriade-groupe.com>
2024-11-28 10:13:07 +0000

    changement couleur surface

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
-    colorScheme:  const ColorScheme.dark(
-      surface: Color.fromARGB(240, 19, 19, 19),
+    colorScheme: const ColorScheme.dark(
+      surface: Color.fromARGB(255, 0, 20, 43),
       primary: Color.fromARGB(255, 174, 174, 174),
       secondary: Color.fromARGB(255, 30, 30, 30),
       tertiary: Color.fromARGB(255, 47, 47, 47),
       inversePrimary: Color.fromRGBO(243, 243, 243, 1.0),
-
     ),
     fontFamily: "Poppins");
